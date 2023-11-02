40e5d3fe3fa4a93e4109e1ec4c531a0cd45e7fb0
SamarKaranch <samarkaranch@gmail.com>
2023-11-02 17:43:49 -0400

    Testing Database

--- focus-flow/src/app/page.tsx
+++ focus-flow/src/app/page.tsx
@@ -1,7 +1,23 @@
 import Image from 'next/image'
+import { db } from './firebase/firebase'
+import { getDocs, collection } from 'firebase/firestore'
 
-export default function Home() {
+export default async function Home() {
   console.log("hello")
+  const usersCollectionRef = collection(db, "users");
+
+  try {
+    const data = await getDocs(usersCollectionRef);
+    const filteredData = data.docs.map((doc) => ({
+      ...doc.data(), 
+      id: doc.id,
+
+    }));
+    console.log(filteredData);
+  } catch (error) {
+    console.log(error)
+  }
+  
   return (
     <div>
       <p>Hi</p>
