08808b2234dd74608dcd26339de93d0e6d985c9a
Colin Le <colintranle123@gmail.com>
2023-11-04 14:16:56 -0400

    Created SignUp and Login

---
+++ focus-flow/src/app/components/Login.tsx
@@ -0,0 +1,9 @@
+function Login() {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default Login;
---
+++ focus-flow/src/app/components/SignUp.tsx
@@ -0,0 +1,9 @@
+function SignUp() {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default SignUp;
