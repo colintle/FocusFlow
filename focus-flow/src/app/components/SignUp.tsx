67fc873d9864c5cc01593fd6f711c4a3743ba64a
NobleTrooper <realxkhan@gmail.com>
2023-11-04 16:29:04 -0400

    Added show/hide functionally for password fields

--- focus-flow/src/app/components/SignUp.tsx
+++ focus-flow/src/app/components/SignUp.tsx
@@ -4,10 +4,12 @@ function SignUp() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
     const handleSignUp = () => {
         // You can add your sign-up logic here.
-        // For simplicity, we're just printing the entered email and password.
+        // For simplicity, we're just printing the entered email and passwords.
         console.log('Email:', email);
         console.log('Password:', password);
         console.log('Confirm Password:', confirmPassword);
@@ -16,7 +18,7 @@ function SignUp() {
     return (
         <div>
             <div>
-               Create your Account:
+                Sign Up Information
             </div>
             <div>
                 <label htmlFor="signupEmail">Email</label>
@@ -27,20 +29,60 @@ function SignUp() {
                     onChange={(e) => setEmail(e.target.value)}
                 /><br />
                 <label htmlFor="signupPassword">Password</label>
-                <input
-                    type="password"
-                    id="signupPassword"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                /><br />
+                <div style={{ position: 'relative' }}>
+                    <input
+                        type={showPassword ? 'text' : 'password'}
+                        id="signupPassword"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {password && (
+                        <button
+                            onClick={() => setShowPassword(!showPassword)}
+                            type="button"
+                            style={{
+                                position: 'absolute',
+                                right: '5px',
+                                top: '50%',
+                                transform: 'translateY(-50%)',
+                                padding: '0',
+                                border: 'none',
+                                background: 'none',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            {showPassword ? 'Hide' : 'Show'}
+                        </button>
+                    )}
+                </div><br />
                 <label htmlFor="confirmPassword">Confirm Password</label>
-                <input
-                    type="password"
-                    id="confirmPassword"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                /><br />
-                <button onClick={handleSignUp}>Sign Up</button>
+                <div style={{ position: 'relative' }}>
+                    <input
+                        type={showConfirmPassword ? 'text' : 'password'}
+                        id="confirmPassword"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                    {confirmPassword && (
+                        <button
+                            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                            type="button"
+                            style={{
+                                position: 'absolute',
+                                right: '5px',
+                                top: '50%',
+                                transform: 'translateY(-50%)',
+                                padding: '0',
+                                border: 'none',
+                                background: 'none',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            {showConfirmPassword ? 'Hide' : 'Show'}
+                        </button>
+                    )}
+                </div><br />
+                <button onClick={handleSignUp}>Create Account</button>
             </div>
         </div>
     );
