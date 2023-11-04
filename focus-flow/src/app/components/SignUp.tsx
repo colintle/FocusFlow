ff33fe8ef7e3ca6b1cf7e98f95fa00c153e9315d
NobleTrooper <realxkhan@gmail.com>
2023-11-04 17:02:57 -0400

    Added tailwind css to SignUp.tsx

--- focus-flow/src/app/components/SignUp.tsx
+++ focus-flow/src/app/components/SignUp.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 function SignUp() {
+    const [fullName, setFullName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
@@ -9,80 +10,82 @@ function SignUp() {
 
     const handleSignUp = () => {
         // You can add your sign-up logic here.
-        // For simplicity, we're just printing the entered email and passwords.
+        // For simplicity, we're just printing the entered full name, email, and passwords.
+        console.log('Full Name:', fullName);
         console.log('Email:', email);
         console.log('Password:', password);
         console.log('Confirm Password:', confirmPassword);
     };
 
     return (
-        <div>
-            <div>
-                Sign Up Information
-            </div>
-            <div>
-                <label htmlFor="signupEmail">Email</label>
-                <input
-                    type="text"
-                    id="signupEmail"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                /><br />
-                <label htmlFor="signupPassword">Password</label>
-                <div style={{ position: 'relative' }}>
+        <div className="bg-gray-200 p-4 rounded-md shadow-md max-w-md mx-auto">
+            <div className="text-2xl text-center mb-4">Create an Account</div>
+            <div className="space-y-4">
+                <div>
+                    <label htmlFor="signupFullName">Full Name</label>
+                    <input
+                        type="text"
+                        id="signupFullName"
+                        value={fullName}
+                        onChange={(e) => setFullName(e.target.value)}
+                        className="w-full px-3 py-2 border rounded"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="signupEmail">Email</label>
+                    <input
+                        type="text"
+                        id="signupEmail"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="w-full px-3 py-2 border rounded"
+                    />
+                </div>
+                <div className="relative">
+                    <label htmlFor="signupPassword">Password</label>
                     <input
                         type={showPassword ? 'text' : 'password'}
                         id="signupPassword"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                        className="w-full px-3 py-2 border rounded"
                     />
                     {password && (
                         <button
                             onClick={() => setShowPassword(!showPassword)}
-                            type="button"
-                            style={{
-                                position: 'absolute',
-                                right: '5px',
-                                top: '50%',
-                                transform: 'translateY(-50%)',
-                                padding: '0',
-                                border: 'none',
-                                background: 'none',
-                                cursor: 'pointer',
-                            }}
+                            className="absolute top-1/2 right-4 -translate-y-1/2 text-blue-500"
                         >
                             {showPassword ? 'Hide' : 'Show'}
                         </button>
                     )}
-                </div><br />
-                <label htmlFor="confirmPassword">Confirm Password</label>
-                <div style={{ position: 'relative' }}>
+                </div>
+                <div className="relative">
+                    <label htmlFor="confirmPassword">Confirm Password</label>
                     <input
                         type={showConfirmPassword ? 'text' : 'password'}
                         id="confirmPassword"
                         value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
+                        className="w-full px-3 py-2 border rounded"
                     />
                     {confirmPassword && (
                         <button
                             onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                            type="button"
-                            style={{
-                                position: 'absolute',
-                                right: '5px',
-                                top: '50%',
-                                transform: 'translateY(-50%)',
-                                padding: '0',
-                                border: 'none',
-                                background: 'none',
-                                cursor: 'pointer',
-                            }}
+                            className="absolute top-1/2 right-4 -translate-y-1/2 text-blue-500"
                         >
                             {showConfirmPassword ? 'Hide' : 'Show'}
                         </button>
                     )}
-                </div><br />
-                <button onClick={handleSignUp}>Create Account</button>
+                </div>
+                <button
+                    onClick={handleSignUp}
+                    className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 cursor-pointer"
+                >
+                    Sign Up
+                </button>
+                <p className="text-gray-600 text-sm text-center">
+                    By signing up you agree to our terms & privacy policy.
+                </p>
             </div>
         </div>
     );
