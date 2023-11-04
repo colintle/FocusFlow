d96f80c0f5b9035cea3cfc2beac5f0a0a32c582b
NobleTrooper <realxkhan@gmail.com>
2023-11-04 16:12:17 -0400

    Updated SignUp.tsx

--- focus-flow/src/app/components/SignUp.tsx
+++ focus-flow/src/app/components/SignUp.tsx
@@ -1,7 +1,47 @@
+import React, { useState } from 'react';
+
 function SignUp() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleSignUp = () => {
+        // You can add your sign-up logic here.
+        // For simplicity, we're just printing the entered email and password.
+        console.log('Email:', email);
+        console.log('Password:', password);
+        console.log('Confirm Password:', confirmPassword);
+    };
+
     return (
         <div>
-            SignUp
+            <div>
+               Sign Up
+            </div>
+            <div>
+                <label htmlFor="signupEmail">Email</label>
+                <input
+                    type="text"
+                    id="signupEmail"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                /><br />
+                <label htmlFor="signupPassword">Password</label>
+                <input
+                    type="password"
+                    id="signupPassword"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                /><br />
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                    type="password"
+                    id="confirmPassword"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                /><br />
+                <button onClick={handleSignUp}>Sign Up</button>
+            </div>
         </div>
     );
 }
