34771bf726354d396d1fcf8e07f77ecd0e455456
NobleTrooper <realxkhan@gmail.com>
2023-11-04 16:06:50 -0400

    Update login.tsx

--- focus-flow/src/app/components/Login.tsx
+++ focus-flow/src/app/components/Login.tsx
@@ -1,14 +1,37 @@
+import React, { useState } from 'react';
+
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        // You can add your login logic here.
+        // For simplicity, we're just printing the entered email and password.
+        console.log('Email:', email);
+        console.log('Password:', password);
+    };
+
     return (
         <div>
             <div>
-                Login
+               Welcome Back
             </div>
             <div>
-                <label for="loginName">Email</label>
-                <input type="text" id="loginName" /><br />
-                <label for="loginPassword">Password</label>
-                <input type="password" id="loginPassword" /><br />
+                <label htmlFor="loginName">Email</label>
+                <input
+                    type="text"
+                    id="loginName"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                /><br />
+                <label htmlFor="loginPassword">Password</label>
+                <input
+                    type="password"
+                    id="loginPassword"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                /><br />
+                <button onClick={handleLogin}>Login</button>
             </div>
         </div>
     );
