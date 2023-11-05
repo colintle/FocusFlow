53d361f5ea7892b5f93011102cb3b8a93a28fa3a
SamarKaranch <samarkaranch@gmail.com>
2023-11-05 09:30:28 -0500

    Finished SignUp API

--- focus-flow/src/app/api/signup/route.tsx
+++ focus-flow/src/app/api/signup/route.tsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, AuthError, Auth } from 'firebase/auth';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { db, auth } from '@/app/firebase/firebase';
 import { setDoc, doc, collection } from 'firebase/firestore';
 import { NextResponse } from "next/server";
@@ -6,17 +6,18 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const data = await request.json();
   const { email, password, firstName, lastName } = data;
-  console.log(data);
+  let errorCode = null;
+  let errorMessage = null;
 
   try {
     // Create a user using the Firebase auth object
-    const userCredential = await createUserWithEmailAndPassword(auth as Auth, email, password);
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    // Signed up
+    const user = userCredential.user;
+    const id = user.uid;
+    console.log(user.uid);
 
-    // Extract the user's UID
-    const uid = userCredential.user.uid;
-
-    // Store additional user information (first name, last name) in Firestore
-    const userRef = doc(collection(db, 'users'), uid);
+    const userRef = doc(collection(db, 'users'), id);
     await setDoc(userRef, {
       firstName,
       lastName,
@@ -24,21 +25,13 @@ export async function POST(request: Request) {
       // Other user properties can be added here
     });
 
-    return NextResponse.json({ message: 'User registered successfully' });
-  } catch (error: any) {
-    let errorMessage = 'An error occurred during registration';
 
-    if (error.code) {
-      switch (error.code) {
-        case 'auth/email-already-in-use':
-          errorMessage = 'The provided email address is already in use.';
-          break;
-        case 'auth/invalid-email':
-          errorMessage = 'The provided email address is not valid.';
-          break;
-        // Add more cases as needed
-      }
-    }
-    return NextResponse.json({ error: errorMessage });
+  } catch (error : any) {
+    errorCode = error.code;
+    errorMessage = error.message;
+    console.log(errorCode + " message: " + errorMessage);
+    return NextResponse.json({ errorCode, errorMessage });
   }
-};
+
+  return NextResponse.json('Registration Successful');
+}
