76d37fb87beddd5803f0cad5797ccb3b39ea5a3d
SamarKaranch <samarkaranch@gmail.com>
2023-11-06 16:14:18 -0500

    Added Login API

--- focus-flow/src/app/api/login/route.tsx
+++ focus-flow/src/app/api/login/route.tsx
@@ -1,3 +1,41 @@
-export async function POST() {
-    return;
+import { signInWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
+import { auth } from '@/app/firebase/firebase';
+import { NextResponse, NextRequest } from "next/server";
+import { cookies } from 'next/headers'
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+  const { email, password } = data;
+  let errorCode = null;
+  let errorMessage = null;
+
+  try {
+    // Sign a user using the Firebase auth object
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+    const user = userCredential.user;
+    await sendEmailVerification(user);
+    
+    if (user.emailVerified) {
+      // The user is logged in only if their email is verified
+      const id = user.uid;
+
+      cookies().set({
+        name: 'Login cookie',
+        value: id,
+        httpOnly: true,
+        path: 'https://focusflow3.vercel.app/home' && 'http://localhost:3000/home',
+        maxAge: 3600,
+        secure: true
+      });
+      
+      return NextResponse.json('Login Successful');
+    } else {
+      return NextResponse.json('Please verify your email before logging in.');
+    }
+  } catch (error: any) {
+    errorCode = error.code;
+    errorMessage = error.message;
+    return NextResponse.json({ errorCode, errorMessage });
+  }
 }
