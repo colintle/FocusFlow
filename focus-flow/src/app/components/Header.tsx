3d9b14f6d38e99e24585af4dfac7ae7a02a06182
Colin Le <colintranle123@gmail.com>
2023-11-10 11:14:38 -0500

    Finished with the popup component

--- focus-flow/src/app/components/Header.tsx
+++ focus-flow/src/app/components/Header.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import {useState} from "react";
 import Image from 'next/image';
 import { FiUser } from "react-icons/fi"
+import Popup from "./Popup";
 
 import logo from '../../../public/logos/ffLogo_slogan.png';
 
 const Header = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const handleClose = () => {
+    setIsPopupOpen(false);
+  };
+
   return (
     <header className="w-full">
       <div className="flex items-center justify-between w-full mx-auto py-2">
@@ -12,9 +22,15 @@ const Header = () => {
         </div>
         
         <div className="flex items-center space-x-4">
-            <FiUser className="h-5 w-5 text-orange-500 hover:text-orange-800 hover:cursor-pointer"/>
+            <FiUser onClick={() => setIsPopupOpen(true)} className="h-5 w-5 text-orange-500 hover:text-orange-800 hover:cursor-pointer"/>
         </div>
       </div>
+      {isPopupOpen && (
+          <Popup title="My Popup" onClose={handleClose}>
+            <p>This is the content of the popup. asdfasdf asdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</p>
+            {/* You can include any other components or HTML elements here as needed */}
+          </Popup>
+        )}
     </header>
   );
 };
