b7c87d4ad44f472e1d54379a9253575fb4c80142
NobleTrooper <realxkhan@gmail.com>
2023-11-15 14:45:10 -0500

    Updated TaskList.tsx

--- focus-flow/src/app/components/TaskList.tsx
+++ focus-flow/src/app/components/TaskList.tsx
@@ -1,11 +1,12 @@
-'use client' 
+'use client';
 import React, { useState } from 'react';
 import Popup from './Popup';
 import { AiOutlinePlus } from 'react-icons/ai';
 import SearchBar from './SearchBar';
 import Task from './Task';
 import Slider from './Slider';
-import image from '../../../public/images/circle-with-i-1.png';
+import image1 from '../../../public/images/circle-with-i-1.png';
+import image2 from '../../../public/images/Description image.png';
 
 function TaskList() {
   const [tasks, setTasks] = useState(["dummyData", "dummyData", "dummyData", "dummyData","dummyData", "dummyData","dummyData", "dummyData"]);
@@ -18,6 +19,16 @@ function TaskList() {
 
   const handleClosePopup = () => {
     setPopupOpen(false);
+  };
+
+  const [checklistItems, setChecklistItems] = useState(["Proident commodo", "Minim laboris non"]);
+  const [newChecklistItem, setNewChecklistItem] = useState("");
+
+  const handleAddMore = () => {
+    if (newChecklistItem.trim() !== "") {
+      setChecklistItems([...checklistItems, newChecklistItem]);
+      setNewChecklistItem(""); // Clear the input field after adding the item
+    }
   };
 
   return (
@@ -45,16 +56,13 @@ function TaskList() {
           onClose={handleClosePopup}
         >
           <div className="flex items-center">
-            <img
-              src={image.src}
-              alt="Image Description"
-              className="mr-4 w-5 h-5 rounded-full mb-20"
-            />
-            <div className="text-left ml-2">
+           
+          
+            <div className="text-left ml-2 flex-grow">
               <p className="font-bold mb-4 mt-2">General Info</p>
               <div className="flex">
                 <div className="mr-4">
-                  <select className="border rounded-md p-1 w-32">
+                  <select className="border p-1 w-32 border-gray-300 focus:outline-none focus:border-blue-500">
                     <option disabled selected hidden>Status</option>
                     <option value="todo">To Do</option>
                     <option value="inprogress">In Progress</option>
@@ -62,7 +70,7 @@ function TaskList() {
                   </select>
                 </div>
                 <div className="mr-4">
-                  <select className="border rounded-md p-1 w-32">
+                  <select className="border p-1 w-32 border-gray-300 focus:outline-none focus:border-blue-500">
                     <option disabled selected hidden>Priority</option>
                     <option value="easy">Easy</option>
                     <option value="medium">Medium</option>
@@ -70,21 +78,58 @@ function TaskList() {
                   </select>
                 </div>
                 <div>
-                  <select className="border rounded-md p-1 w-40">
+                  <select className="border p-1 w-40 border-gray-300 focus:outline-none focus:border-blue-500">
                     <option disabled selected hidden>MM/DD/YYYY</option>
                     {/* Add your options for Due Date here */}
                   </select>
                 </div>
               </div>
 
-              <p className="font-bold mb-8 mt-8">Description</p>
+              <p className="font-bold mb-8 mt-8 flex items-center">
+                <img
+                  src={image2.src}
+                  alt="Description Image"
+                  className="mr-4 w-5 h-5 rounded-full mb-0"
+                />
+                Description
+              </p>
               <input
                 type="text"
-                className="border rounded-md p-1 w-full"
+                className="border p-2 w-full border-gray-300 focus:outline-none focus:border-blue-500"
                 value={descriptionText}
                 onChange={(e) => setDescriptionText(e.target.value)}
                 onClick={() => setDescriptionText("")} // Clear default text on click
               />
+
+              {/* Checklist Section */}
+              <div className="mt-8">
+                <p className="font-bold mb-4">Checklist</p>
+                {checklistItems.map((item, index) => (
+                  <div key={index} className="flex items-center mb-2">
+                    <input
+                      type="checkbox"
+                      className="mr-2"
+                      // Add event handler if needed
+                    />
+                    <span>{item}</span>
+                  </div>
+                ))}
+                <div className="flex items-center mb-2">
+                  <input
+                    type="text"
+                    className="border p-2 mr-2 flex-grow border-gray-300 focus:outline-none focus:border-blue-500"
+                    placeholder="New item..."
+                    value={newChecklistItem}
+                    onChange={(e) => setNewChecklistItem(e.target.value)}
+                  />
+                  <button
+                    className="bg-orange-300 hover:bg-orange-400 text-white font-bold py-2 px-4"
+                    onClick={handleAddMore}
+                  >
+                    Add More
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </Popup>
