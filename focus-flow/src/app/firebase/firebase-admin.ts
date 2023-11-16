4fac3352302fd8026e9b032065593df598b6cc21
SamarKaranch <samarkaranch@gmail.com>
2023-11-16 10:55:39 -0500

    Create firebase-admin.ts

---
+++ focus-flow/src/app/firebase/firebase-admin.ts
@@ -0,0 +1,11 @@
+import admin from 'firebase-admin';
+
+const serviceAccount = require('../../../focusflow-6cd39-firebase-adminsdk-xb2nc-a6bdacf1d9.json');
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+export { admin };
