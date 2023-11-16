f060d16271f91f619a51e7137ff122dc063c61b6
EvanWrote <145154485+EvanWrote@users.noreply.github.com>
2023-11-16 08:02:09 -0500

    Edit and Delete Backend

--- focus-flow/src/app/api/edit/route.tsx
+++ focus-flow/src/app/api/edit/route.tsx
@@ -1,3 +1,33 @@
-export async function POST() {
-    return;
+import { db } from '@/app/firebase/firebase';
+import { doc, updateDoc} from 'firebase/firestore';
+import { NextResponse, NextRequest } from "next/server";
+import { cookies } from 'next/headers'
+
+export async function POST(request: NextRequest) {
+    const cookieStore = cookies()
+    const idCookie = cookieStore.get('User Cookie')
+    const id = idCookie!.value;
+
+    const data = await request.json() ;
+    const { oldTitle, newTitle, description, due, status } = data;
+    let errorCode = null;
+    let errorMessage = null;
+
+    try {
+        const taskRef = doc(db, 'users', id, 'tasks', oldTitle);
+        await updateDoc(taskRef, {
+            title: newTitle,
+            description: description,
+            due: due,
+            status: status
+          });
+
+        
+    } catch (error: any) {
+        errorCode = error.code;
+        errorMessage = error.message;
+        return NextResponse.json({ errorCode, errorMessage });
+    }
+    
+    return NextResponse.json({message: 'Task Edited'});
 }
