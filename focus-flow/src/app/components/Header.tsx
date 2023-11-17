e6f7c4fa3fd8c1d693b4d5ddbba2b444ff7a54b7
Colin Le <colintranle123@gmail.com>
2023-11-17 01:15:38 -0500

    Added logout and signout as separete for the client and sever

--- focus-flow/src/app/components/Header.tsx
+++ focus-flow/src/app/components/Header.tsx
@@ -1,18 +1,29 @@
 "use client";
 
-import {useState} from "react";
 import Image from 'next/image';
-import { FiUser } from "react-icons/fi"
-import Popup from "./Popup";
+import { CiLogout } from "react-icons/ci";
+import { useRouter } from 'next/navigation';
 
 import logo from '../../../public/logos/ffLogo_slogan.png';
 
 const Header = () => {
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const router = useRouter();
+  async function handleLogOut(){
+    const res = await fetch('api/logout', {
+      method: 'GET',
+      headers: {
+          'Content-Type':'application/json',
+      },
+    })
 
-  const handleClose = () => {
-    setIsPopupOpen(false);
-  };
+    const out = await res.json()
+
+    if (!out.errorCode) {
+        alert("Logged out!")
+        router.push("/");
+        return
+    }
+  }
 
   return (
     <header className="w-full">
@@ -22,15 +33,9 @@ const Header = () => {
         </div>
         
         <div className="flex items-center space-x-4">
-            <FiUser onClick={() => setIsPopupOpen(true)} className="h-5 w-5 text-orange-500 hover:text-orange-800 hover:cursor-pointer"/>
+            <CiLogout onClick={() => handleLogOut()} className="h-5 w-5 text-orange-500 hover:text-orange-800 hover:cursor-pointer"/>
         </div>
       </div>
-      {isPopupOpen && (
-          <Popup title="Logout" onClose={handleClose}>
-            <p>Logout Information</p>
-            {/* You can include any other components or HTML elements here as needed */}
-          </Popup>
-        )}
     </header>
   );
 };
