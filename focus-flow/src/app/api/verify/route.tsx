cb2db745b8c2309bd74cc251b1b76afce2dbcb83
SamarKaranch <samarkaranch@gmail.com>
2023-11-18 10:19:53 -0500

    fixed verifty api and middleware

--- focus-flow/src/app/api/verify/route.tsx
+++ focus-flow/src/app/api/verify/route.tsx
@@ -4,28 +4,31 @@ import { collection, doc, getDoc } from "firebase/firestore";
 import { cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
-    let errorCode = null;
-    let errorMessage = null;
-    const cookieStore = cookies();
-    const idCookie = cookieStore.get('User Cookie');
-    const id = idCookie!.value;
+  let errorCode = null;
+  let errorMessage = null;
+  const cookieStore = cookies();
+  const idCookie = cookieStore.get("User Cookie");
+  const id = idCookie?.value;
 
-    try{
-        const userCollection = collection(db, "users");
-        const user = doc(userCollection, id);
-        const userDoc = await getDoc(user);
-    
-        if (userDoc.exists()) {
-          console.log("Document data:", userDoc.data());
-          return NextResponse.redirect(new URL("/home", request.url));
-        } else {
-          return NextResponse.redirect(new URL("/api/signout", request.url));
-        }
+  try {
+    if (id) {
+      const userCollection = collection(db, "users");
+      const user = doc(userCollection, id);
+      const userDoc = await getDoc(user);
+
+      if (userDoc.exists()) {
+        console.log("Document data:", userDoc.data());
+        return NextResponse.redirect(new URL("/home", request.url));
+      } else {
+        return NextResponse.redirect(new URL("/api/signout", request.url));
+      }
+    } else {
+      NextResponse.redirect(new URL("/api/signout", request.url));
     }
-    catch(error: any){
-        console.log(error)
-        errorCode = error.code;
-        errorMessage = error.message;
-        return NextResponse.json({ error, id });
-    }
+  } catch (error: any) {
+    console.log(error);
+    errorCode = error.code;
+    errorMessage = error.message;
+    return NextResponse.json({ error, id });
   }
+}
